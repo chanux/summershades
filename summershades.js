8cca5ee615769fa7bdf50446f7b1959171c8da72
chanux <chanuka@thinkcube.net>
2013-04-02 20:31:51 +0530

    index.html

---
+++ summershades.js
@@ -0,0 +1,62 @@
+var mark = 0;
+var text = "";
+var red = 0;
+var green = 0;
+var blue = 0;
+var colorcode = "";
+
+function builddiv(color){
+    var div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = color;
+
+    shadebox.appendChild(div);
+}
+
+function summermagic(letterindex){
+    val = letterindex * 10 + Math.floor(letterindex/5);
+    hex = val.toString(16);
+
+    if (hex.length < 2) hex = "0" + hex;
+
+    return hex;
+}
+
+function magician(e){
+    if (!e) var e = window.event; //For IE
+
+    mark = ++mark % 3;
+    text = text + String.fromCharCode(e.charCode)
+    var hex = "";
+
+    if (e.which >= 65 && e.which <= 90){
+        hex = summermagic((e.which + 32) % 97);
+        console.log(hex);
+    }
+    else if(e.which >= 97 && e.which <= 122){
+        hex = summermagic(e.which % 97);
+        console.log(hex);
+    }
+
+    if (mark == 1) {
+        red = hex;
+    }
+    else if (mark == 2) {
+        green = hex;
+    }
+    else if (mark == 0){
+        blue = hex;
+        colorcode = "#" + red + green + blue;
+        builddiv(colorcode);
+        console.log(text, colorcode);
+        text = "";
+    }
+}
+
+function load(){
+    var input = document.getElementById("nameBox");
+    input.addEventListener("keypress", magician, false);
+}
+
+document.addEventListener("DOMContentLoaded", load, false);
